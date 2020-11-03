3cd2d8d445f465ba974d8659d2922054525f3da8
AlexRazumenko <alex.razumenko@gmail.com>
2020-11-03 02:44:05 +0200

    base structure and entity classes created

---
+++ src/main/java/org/topjava/alex/entity/Role.java
@@ -0,0 +1,6 @@
+package org.topjava.alex.entity;
+
+public enum Role {
+    USER,
+    ADMIN
+}
---
+++ src/main/java/org/topjava/alex/util/MealsUtil.java
@@ -0,0 +1,7 @@
+package org.topjava.alex.util;
+
+public class MealsUtil {
+
+    public static final int DEFAULT_CALORIES_PER_DAY = 2000;
+
+}
