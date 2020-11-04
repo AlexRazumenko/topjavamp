d840f0c753e6494fabfadf2ceaa07fb84fab1681
AlexRazumenko <alex.razumenko@gmail.com>
2020-11-04 19:47:28 +0200

    MealTo classe created

--- src/main/java/org/topjava/alex/util/MealsUtil.java
+++ src/main/java/org/topjava/alex/util/MealsUtil.java
@@ -1,7 +1,41 @@
 package org.topjava.alex.util;
 
+import org.topjava.alex.to.MealTo;
+import org.topjava.alex.entity.Meal;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.*;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
 public class MealsUtil {
 
     public static final int DEFAULT_CALORIES_PER_DAY = 2000;
+
+    public static final List<Meal> meals = Arrays.asList(
+            new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 10, 0), "Завтрак", 500),
+            new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 13, 0), "Обед", 1000),
+            new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 20, 0), "Ужин", 500),
+            new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 0, 0), "Еда на граничное значение", 100),
+            new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 10, 0), "Завтрак", 1000),
+            new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 13, 0), "Обед", 500),
+            new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 20, 0), "Ужин", 410)
+    );
+
+    public static List<MealTo> filterByPredicate(Collection<Meal> meals, int caloriesPerDay, Predicate<Meal> filter) {
+            final Map<LocalDate, Integer> caloriesSumByDate = new HashMap<>();
+            meals.forEach((meal) -> caloriesSumByDate.merge(meal.getDate(), meal.getCalories(), Integer::sum));
+
+            return meals.stream().filter(filter)
+                    .map(meal -> createTo(meal, caloriesSumByDate.get(meal.getDate()) > caloriesPerDay))
+                    .collect(Collectors.toList());
+    }
+
+
+    private static MealTo createTo(Meal meal, boolean excess) {
+        return new MealTo(meal.getId(), meal.getDateTime(), meal.getDescription(), meal.getCalories(), excess);
+    }
 
 }
